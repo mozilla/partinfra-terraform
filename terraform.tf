815f9cc09477154394c17e3d05080e6a185a2ab0
Yousef Alam <flamingspaz96@googlemail.com>
2016-05-06 18:39:55 +0100

    Add S3 remote state sync - Move meta resources to terraform.tf - Enable versioning on the S3 bucket - Add Terraform remote state

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,26 @@
-resource "aws_s3_bucket" "partinfra-tfsync" {
-    bucket = "partinfra-tfsync"
-    acl = "private"
+variable "tf-sync-bucket" {}
 
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+resource "terraform_remote_state" "s3" {
+    backend    = "s3"
+    config {
+        bucket = "${var.tf-sync-bucket}"
+        key    = "network/terraform.tfstate"
+        region = "${var.aws_region}"
+    }
+}
+
+resource "aws_s3_bucket" "partinfra-tfsync" {
+    bucket          = "${var.tf-sync-bucket}"
+    acl             = "private"
+    versioning {
+        enabled     = true
+    }
     tags {
-        Name = "partinfra-tfsync"
+        Name        = "${var.tf-sync-bucket}"
         Environment = "shared"
     }
 }
