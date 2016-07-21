351742f326e7ccb2e84cd25d6f033ccf5550216e
Yousef Alam <flamingspaz96@googlemail.com>
2016-07-21 14:32:44 +0100

    Add activate.mozilla.community CloudFront module

---
+++ distributions.tf
@@ -0,0 +1,10 @@
+module "activate-mozilla-community" {
+  source              = "git://github.com/mozilla/partinfra-terraform-cloudfrontssl.git"
+
+  origin_domain_name  = "mozilla.github.io"
+  origin_path         = "/activate.mozilla.community"
+  origin_id           = "gh-pages-activate-mozilla-community"
+  alias               = "activate.mozilla.community"
+  comment             = "Bug 1287738"
+  acm_certificate_arn = "arn:aws:acm:${var.aws_region}:${var.aws_account_id}:certificate/1af91a2d-8fa2-4726-abbd-f321b7a136c3"
+}
