de4f2c54c3ef5d97ceffac782245e547708ca5c3
John Giannelos <johngiannelos@gmail.com>
2016-10-27 17:54:43 +0200

    Add mount targets for marathon EFS on admin node.

--- storage.tf
+++ storage.tf
@@ -85,3 +85,56 @@ resource "aws_efs_mount_target" "jenkins-public-efs-shared-1d" {
   subnet_id = "${aws_subnet.apps-shared-1d.id}"
   security_groups = ["${aws_security_group.jenkins-public-efs-sg.id}"]
 }
+
+# EFS mount targets for `marathon-efs` backups
+
+resource "aws_security_group" "marathon-admin-efs-sg" {
+    name = "marathon-admin-efs-sg"
+    description = "Marathon admin EFS SG for backups"
+    vpc_id = "${aws_vpc.apps-shared-vpc.id}"
+}
+
+resource "aws_security_group_rule" "marathon-admin-efs-sg-allownfsfromadmin" {
+    type              = "ingress"
+    from_port         = 2049
+    to_port           = 2049
+    protocol          = "tcp"
+    source_security_group_id = "${aws_security_group.admin-ec2-sg.id}"
+    security_group_id = "${aws_security_group.marathon-admin-efs-sg.id}"
+}
+
+resource "aws_efs_mount_target" "marathon-prod-efs-admin-1a" {
+    file_system_id = "${module.mesos-cluster-production.marathon-efs-id}"
+    subnet_id = "${aws_subnet.apps-shared-1a.id}"
+    security_groups = ["${aws_security_group.marathon-admin-efs-sg.id}"]
+}
+
+resource "aws_efs_mount_target" "marathon-prod-efs-admin-1c" {
+    file_system_id = "${module.mesos-cluster-production.marathon-efs-id}"
+    subnet_id = "${aws_subnet.apps-shared-1c.id}"
+    security_groups = ["${aws_security_group.marathon-admin-efs-sg.id}"]
+}
+
+resource "aws_efs_mount_target" "marathon-prod-efs-admin-1d" {
+    file_system_id = "${module.mesos-cluster-production.marathon-efs-id}"
+    subnet_id = "${aws_subnet.apps-shared-1d.id}"
+    security_groups = ["${aws_security_group.marathon-admin-efs-sg.id}"]
+}
+
+resource "aws_efs_mount_target" "marathon-staging-efs-admin-1a" {
+    file_system_id = "${module.mesos-cluster-staging.marathon-efs-id}"
+    subnet_id = "${aws_subnet.apps-shared-1a.id}"
+    security_groups = ["${aws_security_group.marathon-admin-efs-sg.id}"]
+}
+
+resource "aws_efs_mount_target" "marathon-staging-efs-admin-1c" {
+    file_system_id = "${module.mesos-cluster-staging.marathon-efs-id}"
+    subnet_id = "${aws_subnet.apps-shared-1c.id}"
+    security_groups = ["${aws_security_group.marathon-admin-efs-sg.id}"]
+}
+
+resource "aws_efs_mount_target" "marathon-staging-efs-admin-1d" {
+    file_system_id = "${module.mesos-cluster-staging.marathon-efs-id}"
+    subnet_id = "${aws_subnet.apps-shared-1d.id}"
+    security_groups = ["${aws_security_group.marathon-admin-efs-sg.id}"]
+}
