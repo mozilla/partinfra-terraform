629895260bf3a4e679fd42521c09ed39fdf72d75
Yousef Alam <flamingspaz96@googlemail.com>
2017-06-09 02:51:50 +0100

    Move remo staging redis to prod VPC

--- remo.tf
+++ remo.tf
@@ -2,10 +2,10 @@ module "remo-staging" {
     source                              = "./modules/remo"
 
     environment                         = "staging"
-    vpc_id                              = "${aws_vpc.apps-staging-vpc.id}"
+    vpc_id                              = "${aws_vpc.apps-production-vpc.id}"
     remo_elasticache_instance_size      = "cache.t2.micro"
-    service_security_group_id           = "${module.mesos-cluster-staging.mesos-cluster-slave-sg-id}"
-    elasticache_subnet_group            = "${aws_elasticache_subnet_group.elasticache-staging-subnet-group.name}"
+    service_security_group_id           = "${module.mesos-cluster-production.mesos-cluster-slave-sg-id}"
+    elasticache_subnet_group            = "${aws_elasticache_subnet_group.elasticache-production-subnet-group.name}"
 }
 
 module "remo-production" {
