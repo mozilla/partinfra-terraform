e2addea6307e59767454a7f3e12434752802a777
Yousef Alam <flamingspaz96@googlemail.com>
2017-06-20 14:11:19 +0100

    Add IAM policy for Mozillians

---
+++ modules/mozillians/iam.tf
@@ -0,0 +1,54 @@
+data "aws_iam_policy_document" "mozillians-bucket-policy" {
+    statement {
+        effect = "Allow"
+        actions = [
+            "es:*"
+        ]
+
+        resources = [
+            "${var.elasticsearch_arn}",
+            "${var.elasticsearch_arn}/*"
+        ]
+    }
+
+    statement {
+        effect = "Allow"
+        actions = [
+            "ses:SendRawEmail"
+        ]
+
+        resources = [
+            "*"
+        ]
+    }
+
+    statement {
+        effect = "Allow"
+        actions = [
+            "s3:PutObject"
+        ]
+
+        resources = [
+            "${aws_s3_bucket.exports-bucket.arn}/*"
+        ]
+    }
+
+    statement {
+        effect = "Allow"
+        actions = [
+            "s3:PutObject"
+        ]
+
+        resources = [
+            "${aws_s3_bucket.exports-bucket.arn}"
+        ]
+    }
+}
+
+# Note: This only creates the IAM policy, it needs to be attached to a user or role
+resource "aws_iam_policy" "aws-access-policy" {
+  name        = "mozillians-${var.environment}-s3-ses-es"
+  path        = "/"
+  description = "Mozillians ${var.environment} IAM policy for S3/SES/ES"
+  policy = "${data.aws_iam_policy_document.mozillians-bucket-policy.json}"
+}
