e5b44eaa55b6fd5c8d32dc40056f9e4572be2e9b
John Giannelos <jgiannelos@mozilla.com>
2018-08-02 12:55:57 +0200

    Hardcode AWS region for mcws confluence server.

--- confluence.tf
+++ confluence.tf
@@ -58,6 +58,7 @@ resource "aws_security_group_rule" "confluence-public-allow-all-egress-udp" {
 }
 
 resource "aws_instance" "mcws-confluence" {
+    provider          = "aws.us-east-1"
     ami               = "ami-759bc50a"
     instance_type     = "t2.medium"
     key_name          = "ansible"
