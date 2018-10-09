1ccd58774285a754d2d823351a9412690bfb022a
John Giannelos <jgiannelos@mozilla.com>
2018-10-09 10:03:58 +0200

    Reconcile mysql engine version for RDS instances.

--- db.tf
+++ db.tf
@@ -73,7 +73,7 @@ resource "aws_security_group_rule" "shared-rds-sg-allowpostgresfromstaging" {
 resource "aws_db_instance" "mysql-shared-db" {
     allocated_storage    = 40
     engine               = "mysql"
-    engine_version       = "5.6.40"
+    engine_version       = "5.6.27"
     instance_class       = "db.t2.small"
     publicly_accessible  = false
     backup_retention_period = 7
@@ -159,7 +159,7 @@ resource "aws_security_group_rule" "mozillians-rds-sg-allowmysqlfromprod" {
 resource "aws_db_instance" "mysql-mozillians-db" {
     allocated_storage    = 5
     engine               = "mysql"
-    engine_version       = "5.6.27"
+    engine_version       = "5.6.40"
     instance_class       = "db.t2.medium"
     publicly_accessible  = false
     backup_retention_period = 14
